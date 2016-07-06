a1d234d51dfc68c819fa0b645de6a8a7879bcd7a
Daniela Guillen <deg2158@columbia.edu>
2016-07-06 11:52:27 -0400

    Initial Commit

---
+++ Voice Recorder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Voice Recorder
+//
+//  Created by Daniela Guillen on 7/6/16.
+//  Copyright © 2016 Daniela Guillen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Voice Recorder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Voice Recorder
+//
+//  Created by Daniela Guillen on 7/6/16.
+//  Copyright © 2016 Daniela Guillen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
